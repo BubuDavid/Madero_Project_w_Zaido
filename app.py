b34668f3d9c905d0ddbf0154da4eda7f6d1e7679
David Pedroza Segoviano <david.pedroza.segoviano@gmail.com>
2019-09-02 00:33:13 -0500

    Ya quedó chido el Login y el agregar y borrar productos

--- app.py
+++ app.py
@@ -1,29 +1,80 @@
 from db_functions import db_connect, db_insert_user, db_find_all, db_delete_one
 from db_functions import MONGO_URI
 from forms import BuyForm, LoginForm
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
+import os
 
 app = Flask(__name__, template_folder = 'templates')
 
-users = db_connect(MONGO_URI, 'Project_name', 'Collection_Name')
+inventario = db_connect(MONGO_URI, 'Proyecto_Madero', 'inventario')
+usuarios = db_connect(MONGO_URI, 'Proyecto_Madero', 'usuarios')
+
+app.secret_key = os.urandom(24)
 
 @app.route('/', methods=['GET', 'POST'])
-def login():
+def index():
     form = BuyForm(request.form)
-    items = db_find_all(users)
+
+    if 'user' in session:
+        session_flag = True
+    else:
+        session_flag = False
     
     if request.method == 'POST':
         item = form.item.data
         precio = form.precio.data
-        if item != None and precio != None:
-            coso = {
-                "item": item,
-                "precio": precio
-            }
-            db_insert_user(users, coso)
-            items = db_find_all(users)
+
+        if request.form['submit_button'] == 'Salir':
+            session.pop('user', None)
+            return redirect('/')
+        if request.form['submit_button'] == 'Borrar':
+            if item != None and precio != None:
+                coso = {
+                    'item': item,
+                    'precio': precio
+                }
+                db_delete_one(inventario, coso)
+        else:
+            if item != None and precio != None:
+                coso = {
+                    "item": item,
+                    "precio": precio
+                }
+                db_insert_user(inventario, coso)
+
+    items = db_find_all(inventario)
             
-    return render_template('admin.html', items=items)
+    return render_template('admin.html', items=items, session_flag=session_flag)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    error = False
+
+    if request.method == 'POST':
+        username = form.username.data
+        password = form.password.data
+
+        print(username)
+        print(password)
+
+        if username != None and password != None:
+            usuario = {
+                'username': username,
+                'password': password
+            }
+
+            trying = db_find_all(usuarios, usuario)
+
+            try:
+                for tried in trying:
+                    tried['username']
+                    session['user'] = username
+                    return redirect('/')
+            finally:
+                error = True
+
+    return render_template('login.html', error = error)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, port=8000)
